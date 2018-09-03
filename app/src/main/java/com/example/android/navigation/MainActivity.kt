f3cf798d59ed4b18266084a69f62abbf61d92b14
Dan Galpin <dgalpin@google.com>
2018-09-03 02:16:30 -0700

    Step.09-Exercise-Adding-the-Navigation-Drawer Because it's one cool navigation pattern --- for complex apps.

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -28,17 +28,22 @@ import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // TODO (05) Add private lateinit var drawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        // TODO (06) Initialize drawerLayout var from binding
 
         val navController = this.findNavController(R.id.myNavHostFragment)
+        // TODO (07) Add the DrawerLayout as the second parameter to setupActionBarWithNavController
         NavigationUI.setupActionBarWithNavController(this, navController)
+        // TODO (08) Hook the navigation UI up to the navigation view. (navView)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
+        // TODO (09) Replace navController.navigateUp with NavigationUI.navigateUp with drawerLayout param
         return navController.navigateUp()
     }
 }
